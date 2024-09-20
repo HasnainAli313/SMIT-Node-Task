c476d1968f07986558dd4982b62af76a822fccb3
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-20 22:02:45 +0500

    added POST method

--- methods.mjs
+++ methods.mjs
@@ -10,8 +10,24 @@ const server = createServer((req,res)=>{
 
     res.setHeader('Content-Type', 'application/json');
 
+    //GET Method
     if(method === 'GET' && parsedURL.pathname === '/api/items'){
         res.statusCode = 200;
         res.end(JSON.stringify({message:'GET Request- Fetching data'}))
     }
+
+    //POST Method
+    else if(method === 'POST' && parsedURL.pathname === '/api/items'){
+        let body = "";
+        req.on('data',chunk => {
+            body += chunk.toString();
+        });
+        res.on('end',() =>{
+            const newItem = JSON.parse(body);
+            res.statusCode = 201;
+            res.end(JSON.stringify({message:'POST Request- Creating new item',data:newItem}))
+        });
+    
+    }
+
 })
