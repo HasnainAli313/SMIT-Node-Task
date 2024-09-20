9eea79df2547a392245783a5a27d598e56d5c3de
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-20 21:57:18 +0500

    added get method

--- methods.mjs
+++ methods.mjs
@@ -4,5 +4,14 @@ const PORT = 3000;
 
 const server = createServer((req,res)=>{
 
-  
+    // getting from req 
+    const {method ,url} = req;
+    const parsedURL = new URL(url,`http://${req.headers.hot}`);
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if(method === 'GET' && parsedURL.pathname === '/api/items'){
+        res.statusCode = 200;
+        res.end(JSON.stringify({message:'GET Request- Fetching data'}))
+    }
 })
